fa58836b6e44a0d48d7b699d96d1f26133997802
DevinChong <truong.kevin.81824@gmail.com>
2018-04-26 15:51:32 -0700

    Drew board

--- Minesweeper3D/src/MinesUI.java
+++ Minesweeper3D/src/MinesUI.java
@@ -1,4 +1,5 @@
 import java.awt.EventQueue;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
@@ -6,13 +7,20 @@ import javax.swing.JPanel;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
 import java.awt.Panel;
+import java.awt.geom.Path2D;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Dimension;
+import javax.swing.JToolBar;
 
 public class MinesUI {
 
+	private int cells;
 	private JFrame frame;
+	private Graphics graphics;
 
 	/**
 	 * Launch the application.
@@ -21,7 +29,7 @@ public class MinesUI {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MinesUI window = new MinesUI();
+					MinesUI window = new MinesUI(15);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -33,7 +41,8 @@ public class MinesUI {
 	/**
 	 * Create the application.
 	 */
-	public MinesUI() {
+	public MinesUI(int cell) {
+		cells=cell;
 		initialize();
 	}
 
@@ -42,7 +51,7 @@ public class MinesUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(cells*20, cells*20, 375, 214); //Save: cells*85, cells*35
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JMenuBar menuBar = new JMenuBar();
@@ -60,8 +69,26 @@ public class MinesUI {
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.NORTH);
 		
-		JPanel panel_1 = new JPanel();
+		JPanel panel_1 = new JPanel(){
+			public void paint(Graphics g){
+				graphics=g;
+				for(int i=0;i<cells;i++){
+					for(int j=0;j<cells;j++){
+						drawCell((30*cells)+(j*50)-(i*25),(i*25));
+					}
+				}
+			}
+		};
 		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
+		
 	}
-
+	private void drawCell(int x, int y){
+	    graphics.drawLine(x,y,x+50,y);
+	    graphics.drawLine(x+50,y,x+25,y+25);
+	    graphics.drawLine(x+25,y+25,x-25,y+25);
+	    graphics.drawLine(x-25,y+25,x,y);
+	}
+	public void setCells(int c){
+		cells=c;
+	}
 }
