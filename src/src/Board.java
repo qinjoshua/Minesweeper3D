e0c54f35369ca0b68e330830385b61754565f21b
Alvin Lin <alvinlin88@gmail.com>
2018-04-27 10:00:52 -0700

    Added multiple methods

--- src/src/Board.java
+++ src/src/Board.java
@@ -1,15 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+
 public class Board {
 
     private int width;
     private int length;
     private int height;
     private int numMines;
-
+    private String[][][] dimensions;
 
     public Board(){
         this.width = 9;
         this.length = 9;
         this.height = 9;
+
+        createBoard();
     }
 
     public Board(int width, int length, int height, int numMines){
@@ -17,8 +25,50 @@ public class Board {
         this.length = length;
         this.height = height;
         this.numMines = numMines;
+
+        createBoard();
+    }
+
+    public void scramble(){
+        int placed = 0;
+        Random rand = new Random();
+
+        while(placed < numMines){
+            int x = rand.nextInt(length);
+            int y= rand.nextInt(width);
+            int z = rand.nextInt(height);
+
+            if(dimensions[x][y][z] != "*"){
+                dimensions[x][y][z] = "*";
+                placed++;
+            }
+        }
+    }
+
+    public void createBoard(){
+
+        //initialize 3d Array and set value to default of "-"
+        dimensions = new String[this.length][this.width][this.height];
+        for(int i=0; i<this.length; i++){
+            for(int j=0; j<this.width; j++){
+                for(int k=0; k<this.height; k++){
+                    dimensions[i][j][k] = "-";
+                }
+            }
+        }
+
+        scramble(); //randomly place the mines by calling on the scramble() method
+
+    }
+
+    public void explode(){
+
+    }
+
+    public void generatePointValue(){
+
     }
 
 
+
 }
-s
